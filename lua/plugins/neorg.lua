c582eae82d988b64b336de16c1df668c8c96dd6d
Adrien Barret <adrien@artemislab.io>
2024-03-21 17:05:37 -0400

    adding diff view, neorg

---
+++ lua/plugins/neorg.lua
@@ -0,0 +1,32 @@
+return {
+  "nvim-neorg/neorg",
+  build = ":Neorg sync-parsers",
+  dependencies = { "nvim-lua/plenary.nvim", "nvim-neorg/neorg-telescope" },
+  config = function()
+    require("neorg").setup({
+      load = {
+        ["core.defaults"] = {}, -- Loads default behaviour
+        ["core.concealer"] = {}, -- Adds pretty icons to your documents
+        ["core.dirman"] = { -- Manages Neorg workspaces
+          config = {
+            workspaces = {
+              notes = "~/Documents/notes",
+              work = "~/Documents/notes/work",
+              private = "~/Documents/notes/private",
+            },
+            index = "index.norg",
+            default_workspaces = "work",
+          },
+        },
+        ["core.export"] = {},
+        ["core.summary"] = {},
+        ["core.integrations.telescope"] = {},
+        ["core.keybinds"] = {
+          config = {
+            neorg_leader = " ",
+          },
+        },
+      },
+    })
+  end,
+}
