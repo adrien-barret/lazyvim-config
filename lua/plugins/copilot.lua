31eae18fabe794cb1094dd1eb1c589c7731a2221
Adrien Barret <adrien@artemislab.io>
2024-04-25 16:38:45 -0400

    update copilot and zen-mode

--- lua/plugins/copilot.lua
+++ lua/plugins/copilot.lua
@@ -122,20 +122,4 @@ return {
       })
     end,
   },
-
-  {
-    "CopilotC-Nvim/CopilotChat.nvim",
-    branch = "canary",
-    dependencies = {
-      { "zbirenbaum/copilot.lua" }, -- or github/copilot.vim
-      { "nvim-lua/plenary.nvim" }, -- for curl, log wrapper
-    },
-    opts = {
-      debug = true, -- Enable debugging
-      -- See Configuration section for rest
-    },
-    -- See Commands section for default commands if you want to lazy load on them
-  },
-  -- https://github.com/CopilotC-Nvim/CopilotChat.nvim/discussions/27#discussioncomment-8438000
-  -- https://github.com/CopilotC-Nvim/CopilotChat.nvim/discussions/27
 }
